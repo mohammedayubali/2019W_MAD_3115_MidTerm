e292dc87b10bee0bb97bf652f6c44fabf1f765fa
MacStudent <macstudent@126-imac32.local>
2019-03-20 18:47:01 -0400

    Login screen added

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var SwitchOfOn: UISwitch!
+    @IBOutlet weak var UserPassword: UITextField!
+    @IBOutlet weak var UserText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btn_Click(_ sender: UIButton) {
+        if(UserText == "admin" && UserPassword == "123" )
+        {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let svc =
+        
+    }
 }
 
---
+++ 2019W_MAD_3115_MidTerm/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  2019W_MAD_3115_MidTerm
+//
+//  Created by MacStudent on 2019-03-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct Student {
+    var studentId = 1
+    
+}
